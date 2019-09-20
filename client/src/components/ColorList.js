3549deb9161dcd1d369ef1a50c06368d44d5b4e5
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-20 10:34:12 -0600

    updating color function semi-working

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,10 +22,23 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        updateColors(res.data);
+        window.location =`/protected`;
+      })
+      .catch(err => console.log(err.response));
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`http://localhost:5000/api/colors/${colorToEdit.id}`)
+    .then(res => {
+      updateColors(res.data);
+    })
+    .catch(err => console.log(err.response));
   };
 
   return (
